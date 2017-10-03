38de8b8fb7268603a543be2636f820eec50ed546
Mike Perez <thingee@gmail.com>
2017-10-02 19:17:19 -0700

    Support py3.6 in decode strings and binary types

--- git_os_job/cmd.py
+++ git_os_job/cmd.py
@@ -20,6 +20,50 @@ import subprocess
 import sys
 import webbrowser
 
+import six
+
+
+def safe_decode(text, incoming=None, errors='strict'):
+    """Decodes incoming text/bytes string.
+
+    This is done using `incoming` if they're not already unicode. This code
+    originally came from the module oslo.utils:
+    http://git.openstack.org/cgit/openstack/oslo.utils/tree/oslo_utils/encodeutils.py#n27
+
+    :param incoming: Text's current encoding
+    :param errors: Errors handling policy. See here for valid
+        values http://docs.python.org/3/library/codecs.html
+    :returns: text or a unicode `incoming` encoded
+                representation of it.
+    :raises TypeError: If text is not an instance of str
+    """
+    if not isinstance(text, (six.string_types, six.binary_type)):
+        raise TypeError("%s can't be decoded" % type(text))
+
+    if isinstance(text, six.text_type):
+        return text
+
+    if not incoming:
+        incoming = (sys.stdin.encoding or
+                    sys.getdefaultencoding())
+
+    try:
+        return text.decode(incoming, errors)
+    except UnicodeDecodeError:
+        # Note(flaper87) If we get here, it means that
+        # sys.stdin.encoding / sys.getdefaultencoding
+        # didn't return a suitable encoding to decode
+        # text. This happens mostly when global LANG
+        # var is not set correctly and there's no
+        # default encoding. In this case, most likely
+        # python will use ASCII or ANSI encoders as
+        # default encodings but they won't be capable
+        # of decoding non-ASCII characters.
+        #
+        # Also, UTF-8 is being used since it's an ASCII
+        # extension.
+        return text.decode('utf-8', errors)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -82,20 +126,20 @@ def main():
         url = 'https://review.openstack.org/#/q/commit:' + commit
     else:
         try:
-            ref_hash = subprocess.check_output(
+            ref_hash = safe_decode(subprocess.check_output(
                 ['git', 'show-ref', '-s', ref]
-            ).decode('utf-8').rstrip()
+            )).rstrip()
         except subprocess.CalledProcessError:
             # Maybe they gave us a commit id
             try:
-                ref_hash = subprocess.check_output(
+                ref_hash = safe_decode(subprocess.check_output(
                     ['git', 'show', '--pretty=format:%H', '--quiet', ref]
-                ).decode('utf-8').rstrip()
+                )).rstrip()
             except subprocess.CalledProcessError:
                 sys.stderr.write('Could not get hash for ref %r\n' % ref)
                 return 1
 
-        ref_hash_str = ref_hash.decode('utf8')
+        ref_hash_str = safe_decode(ref_hash)
         url = '%s/%s/%s/' % (args.base, ref_hash_str[:2], ref_hash_str)
 
     if args.url:
